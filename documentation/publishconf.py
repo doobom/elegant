ddd048cb1bd0c831ab83c1d0542936e4d15a9e3f
doobom <rovecat@gmail.com>
2020-02-06 11:01:53 +0800

    merge

--- documentation/publishconf.py
+++ documentation/publishconf.py
@@ -22,6 +22,7 @@ if os.environ.get("CONTEXT") == "production":
     STAT_COUNTER_SECURITY = os.environ.get("STAT_COUNTER_SECURITY_PROD")
     GOOGLE_ANALYTICS = os.environ.get("GOOGLE_ANALYTICS_PROD")
     DISQUS_SITENAME = os.environ.get("DISQUS_SITENAME")
+    COMMENTBOX_PROJECT = os.environ.get("COMMENTBOX_PROJECT")
 
 elif os.environ.get("CONTEXT") == "branch-deploy" and os.environ.get("HEAD") == "next":
     SITENAME = "Elegant (Next)"
@@ -32,6 +33,7 @@ elif os.environ.get("CONTEXT") == "branch-deploy" and os.environ.get("HEAD") == 
     STAT_COUNTER_PROJECT = os.environ.get("STAT_COUNTER_PROJECT_NEXT")
     STAT_COUNTER_SECURITY = os.environ.get("STAT_COUNTER_SECURITY_NEXT")
     GOOGLE_ANALYTICS = os.environ.get("GOOGLE_ANALYTICS_NEXT")
+    COMMENTBOX_PROJECT = os.environ.get("COMMENTBOX_PROJECT")
 
 else:
     SITEURL = ""
